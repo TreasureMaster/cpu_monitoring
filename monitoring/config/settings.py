83aff7aa05a008cde765c75bf5925e39d0eb790c
Alex <whites.russia@gmail.com>
2023-05-03 10:42:30 +0300

    [imp]: запуск планировщика

--- monitoring/config/settings.py
+++ monitoring/config/settings.py
@@ -131,3 +131,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфигурация планировщика
+SCHEDULER_CONFIG = {
+    "apscheduler.jobstores.default": {
+        "class": "django_apscheduler.jobstores:DjangoJobStore"
+    },
+    'apscheduler.executors.processpool': {
+        "type": "threadpool"
+    },
+}
+SCHEDULER_AUTOSTART = True
