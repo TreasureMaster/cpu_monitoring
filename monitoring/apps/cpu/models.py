ed6a6165cf2785176be72711f45c573b32e7655d
Alex <whites.russia@gmail.com>
2023-05-03 10:11:57 +0300

    [add]: модель загрузки процессора

---
+++ monitoring/apps/cpu/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+
+class CpuLoad(models.Model):
+    """Мониторинг загрузки процессора"""
+    created_at = models.DateTimeField(
+        _('Creation date and time'), auto_now_add=True,
+    )
+    value = models.FloatField(
+        _('CPU usage in percent'),
+        default=0.0,
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+    )
